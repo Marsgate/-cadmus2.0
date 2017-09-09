f0910edc3e7f997b57b75343661ca38319b83a60
Marsgate <micahrassi@gmail.com>
2017-09-09 18:42:54 -0400

    added highstack and retract funtions to autostacker

--- Code/src/autostack.c
+++ Code/src/autostack.c
@@ -5,25 +5,58 @@
 #include "ports.h"
 
 #define ENCTARGET 180
-#define POTTARGETLOW 630 //change to actual
+#define POTTARGETLOW 630
+#define POTTARGETHIGH 2100
+#define POTTARGETBOTTOM 400
 
 int timer = 0;
 
 void lowStack(){
   arm(127);
   lift(127);
-    if(encoderGet(armEnc) > ENCTARGET){
-      arm(0);
-    }
 
-    if(analogRead(LIFTPOT) > POTTARGETLOW){
-      lift(0);
-    }
+  if(encoderGet(armEnc) > ENCTARGET){
+    arm(0);
+  }
 
+  if(analogRead(LIFTPOT) > POTTARGETLOW){
+    lift(0);
+  }
 }
+
+void highStack(){
+  arm(127);
+  lift(127);
+
+  if(encoderGet(armEnc) > ENCTARGET){
+    arm(0);
+  }
+
+  if(analogRead(LIFTPOT) > POTTARGETHIGH){
+    lift(0);
+  }
+}
+
+void retract(){
+  arm(-127);
+  lift(-127);
+
+  if(digitalRead(ARM_LIMIT) == LOW){
+    arm(0);
+  }
+
+  if(analogRead(LIFTPOT) > POTTARGETBOTTOM){
+    lift(0);
+  }
+}
+
 
 void autoStack(){
   if(joystickGetDigital(1, 5, JOY_DOWN)){
     lowStack();
+  }else if(joystickGetDigital(1, 5, JOY_UP)){
+    highStack();
+  }else if(joystickGetDigital(1, 6, JOY_UP)){
+    retract();
   }
 }
