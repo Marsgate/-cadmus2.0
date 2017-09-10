a890ad3f60b39fae8a44ea2d5d3afe8431bdb155
Marsgate <micahrassi@gmail.com>
2017-09-09 22:16:18 -0400

    initialized the lcd and deleted comments from init.c

--- Code/src/init.c
+++ Code/src/init.c
@@ -1,47 +1,23 @@
-/** @file init.c
- * @brief File for initialization code
- *
- * This file should contain the user initialize() function and any functions related to it.
- *
- * Any copyright is dedicated to the Public Domain.
- * http://creativecommons.org/publicdomain/zero/1.0/
- *
- * PROS contains FreeRTOS (http://www.freertos.org) whose source code may be
- * obtained from http://sourceforge.net/projects/freertos/files/ or on request.
- */
-
 #include "main.h"
 #include "ports.h"
-/*
- * Runs pre-initialization code. This function will be started in kernel mode one time while the
- * VEX Cortex is starting up. As the scheduler is still paused, most API functions will fail.
- *
- * The purpose of this function is solely to set the default pin modes (pinMode()) and port
- * states (digitalWrite()) of limit switches, push buttons, and solenoids. It can also safely
- * configure a UART port (usartOpen()) but cannot set up an LCD (lcdInit()).
- */
+
 void initializeIO() {
+  //led port
   pinMode(LED, OUTPUT_OD);
 }
 
-/*
- * Runs user initialization code. This function will be started in its own task with the default
- * priority and stack size once when the robot is starting up. It is possible that the VEXnet
- * communication link may not be fully established at this time, so reading from the VEX
- * Joystick may fail.
- *
- * This function should initialize most sensors (gyro, encoders, ultrasonics), LCDs, global
- * variables, and IMEs.
- *
- * This function must exit relatively promptly, or the operatorControl() and autonomous() tasks
- * will not start. An autonomous mode selection menu like the pre_auton() in other environments
- * can be implemented in this task if desired.
- */
+
 void initialize() {
+  //sensors
   armEnc = encoderInit(3, 4, false);
   driveEncLeft = encoderInit(D_ENC_L1, D_ENC_L2, false);
   driveEncRight = encoderInit(D_ENC_R1, D_ENC_R2, true);
 
+  //lcd
+  lcdInit(uart1);
+  lcdClear(uart1);
+
+  //expander light check
 	while(analogRead(EXPANDER) < 500){
 		digitalWrite(LED, LOW);
 		delay(500);
