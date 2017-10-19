c7ef9e7527b36b5be25942543bfb0c690f9a16f0
Marsgate <micahrassi@gmail.com>
2017-10-19 18:38:35 -0400

    Added a rudimentary PID control to the lift
    Not implemented yet. It needs tuned and integrated still.

--- Code/src/lift.c
+++ Code/src/lift.c
@@ -1,6 +1,16 @@
 #include "API.h"
 #include "ports.h"
 
+//define coefficients
+int kp = 1;
+int ki = 1;
+int kd = 1;
+
+//define global variables
+int integral = 0;
+int derivative = 0;
+int prevError = 0;
+
 void lift(int vel){
   motorSet(LIFT1, vel);
   motorSet(LIFT2, vel);
@@ -14,4 +24,13 @@ void liftOp(){
   }else{
     lift(0);
   }
+}
+
+void liftPID(int sp, int sv){
+  int error = sp - sv;
+  integral = integral + error;
+  derivative = error - prevError;
+  prevError = error;
+  int speed = error*kp + integral*ki + derivative*kd;
+  lift(speed);
 }
