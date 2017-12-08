bba3ea66be8aa60ecb5404eed18fdaecb3bd3930
WawaseeLegends <WawaseeLegends@users.noreply.github.com>
2017-12-08 15:56:46 -0500

    Trial Autonomous & function inclusions

--- 574A/src/drive.c
+++ 574A/src/drive.c
@@ -8,6 +8,11 @@ void left(int vel){
   motorSet(DRIVE4, vel);
 }
 
+void drive(int vel){
+  right(vel);
+  left(vel);
+}
+
 void driveOp(){
   left(joystickGetAnalog(2, 3));
   right(joystickGetAnalog(2, 2));
