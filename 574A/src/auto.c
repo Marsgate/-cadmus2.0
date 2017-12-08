bf0644bb0e2a7468a656d4c7ab22152daccd0f16
WawaseeLegends <WawaseeLegends@users.noreply.github.com>
2017-12-08 17:51:11 -0500

    Autonomous Phase 1
    Driver Controls were not touched, autonomous is not tested but is psuedocoded

--- 574A/src/auto.c
+++ 574A/src/auto.c
@@ -6,9 +6,41 @@
 #include "antitip.h"
 
 void autonomous() {
+  lift(127); //lift rises for 2.5 seconds then stops
+  delay(2000);
+  lift(0);
   drive(127);
-  delay(1000);
+  antitip(-127);
+  delay(1250);
+  antitip(0);
+  delay(2250);
   drive(0);
-
+  pylon(127);
+  delay(2500);
+  pylon(0);
+  drive(-127);
+  delay(3000);
+  drive(0);
+  right(127);
+  left(-127);
+  delay(250);
+  right(0);
+  left(0);
+  drive(-127);
+  delay(500);
+  drive(0);
+  right(127);
+  left(-127);
+  delay(250);
+  right(0);
+  left(0);
+  drive(127);
+  delay(1250);
+  drive(0);
+  antitip(127);
+  delay(1100);
+  antitip(0);
+  drive(-127);
+  delay(1250);
   motorStopAll();
 }
