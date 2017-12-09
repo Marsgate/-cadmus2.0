62142b77b879dfefbcd938beb4d62a63436a9d21
WawaseeLegends <WawaseeLegends@users.noreply.github.com>
2017-12-08 19:25:01 -0500

    Autonomous Phase 2
    Autonomous has been tested, scores mobile base in 20 pts zone

--- 574A/src/auto.c
+++ 574A/src/auto.c
@@ -6,41 +6,48 @@
 #include "antitip.h"
 
 void autonomous() {
-  lift(127); //lift rises for 2.5 seconds then stops
-  delay(2000);
-  lift(0);
+  lift(127); //lift rises for 2.5 seconds then stop
   drive(127);
-  antitip(-127);
-  delay(1250);
+  antitip(127);
+  delay(1000);
   antitip(0);
-  delay(2250);
-  drive(0);
-  pylon(127);
-  delay(2500);
-  pylon(0);
-  drive(-127);
-  delay(3000);
-  drive(0);
-  right(127);
-  left(-127);
-  delay(250);
-  right(0);
-  left(0);
-  drive(-127);
+  delay(1000);
+  lift(0);
   delay(500);
   drive(0);
+  delay(100);
+  pylon(127);
+  delay(1500);
+  pylon(0);
+  delay(100);
+  drive(-127);
+  delay(2000);
+  drive(0);
+  delay(100);
   right(127);
   left(-127);
-  delay(250);
+  delay(300);
   right(0);
   left(0);
-  drive(127);
-  delay(1250);
+  delay(100);
+  drive(-127);
+  delay(750);
   drive(0);
-  antitip(127);
+  delay(100);
+  right(127);
+  left(-127);
+  delay(350);
+  right(0);
+  left(0);
+  delay(100);
+  drive(127);
+  delay(1550);
+  drive(0);
+  delay(100);
+  antitip(-127);
   delay(1100);
   antitip(0);
   drive(-127);
-  delay(1250);
+  delay(1450);
   motorStopAll();
 }
