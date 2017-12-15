30a08bfaee8925c70515ea66182eff9dba90c8b5
WawaseeLegends <WawaseeLegends@users.noreply.github.com>
2017-12-15 17:33:16 -0500

    Revert "Maybe Working"
    This reverts commit 06dfbc8616fcb17cf6aa1f28ada2025eecbd8519.

--- 574A/src/auto.c
+++ 574A/src/auto.c
@@ -5,11 +5,7 @@
 #include "pylon.h"
 #include "antitip.h"
 
-<<<<<<< HEAD
 void autonomous() {
-=======
-void leftPylon20() {
->>>>>>> parent of 1611f92... Autonomous Both sides, Phase 2.5 code
   lift(127); //lift rises for 2.5 seconds then stop
   drive(127);
   antitip(127);
@@ -55,49 +51,3 @@ void leftPylon20() {
   delay(1450);
   motorStopAll();
 }
-<<<<<<< HEAD
-=======
-
-void CenterStack(){
-  lift(127);
-  delay(2000);
-  lift(0);
-  delay(100);
-  drive(127);
-  delay(400);
-  drive(0);
-  left(127);
-  right(-127);
-  delay(250);
-  drive(0);
-  delay(100);
-  drive(127);
-  antitip(127);
-  delay(1000);
-  antitip(0);
-  delay(500);
-  drive(0);
-  lift(-127);
-  delay(750);
-  lift(0);
-  claw(-127);
-  delay(250);
-  claw(0);
-  drive(-127);
-  delay(500);
-  drive(0);
-}
-
-void autonomous() {
-  switch(auton){
-    case 0:
-      break; //dont run auto
-    case 1:
-      leftPylon20();
-      break;
-    case 2:
-      CenterStack();
-      break;
-  }
-}
->>>>>>> parent of 1611f92... Autonomous Both sides, Phase 2.5 code
