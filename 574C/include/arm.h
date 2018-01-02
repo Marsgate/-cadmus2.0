5a9b1f73aeaa92e86da6c50c3472df92f1b45a82
Marsgate <micahrassi@gmail.com>
2018-01-02 14:44:25 -0500

    removed other team files

--- 574C/include/arm.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ARM_H_
-#define _ARM_H_
-
-
-void arm(int vel);
-void armTest();
-void armPID(int sv);
-
-#endif
--- 574C/include/lift.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LIFT_H_
-#define _LIFT_H_
-
-void lift(int vel);
-void liftOp();
-void liftPID(int sp);
-
-#endif
